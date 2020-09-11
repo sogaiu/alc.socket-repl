1a4332e47d88470dd6c73bc48d64f78a1e78ace0
sogaiu <983021772@users.noreply.github.com>
2020-09-11 13:49:17 +0900

    Choose suitable port

--- src/alc/socket_repl.clj
+++ src/alc/socket_repl.clj
@@ -1,13 +1,40 @@
 (ns alc.socket-repl
   (:require
     [clojure.main :as cm]
-    [clojure.core.server :as ccs]))
+    [clojure.core.server :as ccs])
+  (:import
+   [java.net InetAddress ServerSocket UnknownHostException]))
+
+(defn check-port
+  [port]
+  (when-let [addr
+             (try
+               ;; XXX: possibly want to allow address specification
+               (InetAddress/getByAddress (bytes (byte-array [127 0 0 1])))
+               (catch UnknownHostException _
+                 ;; XXX
+                 (println "failed to determine localhost address")
+                 nil))]
+    (when-let [^ServerSocket sock
+               (try
+                 (ServerSocket. port 0 addr)
+                 (catch java.io.IOException _
+                   ;; XXX
+                   (println "failed to create socket for:" port)
+                   nil))]
+      (let [received-port (.getLocalPort sock)]
+        (.close sock)
+        received-port))))
+
+(defn find-port
+  []
+  (check-port 0))
 
 ;; XXX: option to write a dot file with port number?
 (defn start
   [{:keys [address name port repl]}]
-  ;; XXX: check that port is a sensible number?
-  (let [port (or port 7789)
+  (let [port (or port (find-port))
+        _ (assert port (str "Failed to obtain port"))
         name (or name (str port))]
     (if-let [prop (System/getProperty "clojure.server.repl")]
       (println "Socket repl detected? " prop)
